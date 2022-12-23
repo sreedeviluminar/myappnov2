43d7fdc8793d2cf34e59a462207b423a165c6c35
Sreedevi <sreedevi@luminartechnolab.com>
2022-12-23 10:05:30 +0530

    commit

---
+++ lib/List with Builder.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: List_with_Builder(),));
+}
+
+class List_with_Builder extends StatelessWidget {
+
+  var name = ['name1', 'name2', 'name3', 'name4', 'name5'];
+  var phone = ['909789687', '09789696', '097987986', '89685875', '968757644'];
+  var images = [
+    "assets/icons/tiger.png",
+    "assets/icons/shell.png",
+    "assets/icons/babydog.png",
+    "assets/icons/tiger.png",
+    "assets/icons/shell.png"
+  ];
+  var colors = [800,600,400,200,100];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Contact book"),),
+        body: ListView.builder(
+          itemCount: name.length,
+            itemBuilder: (BuildContext context, index) {
+              return Card(
+                color: Colors.purple[colors[index]],
+                child: ListTile(
+                  title: Text(name[index]),
+                  subtitle: Text(phone[index]),
+                  leading: Image(image: AssetImage(images[index]),),
+                ),);
+            })
+    );
+  }
+}
+  // var datas = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7'];
+  // var colors = [700,600, 500,400, 300,200, 100];
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(title: Text("LIST WITH BUILDER"),),
+  //     body: ListView.builder(
+  //       itemCount: datas.length,
+  //         itemBuilder: (BuildContext context, int index) {
+  //           return Container(
+  //             height: 50,
+  //             color: Colors.green[colors[index]],
+  //             child: Center(child: Text(datas[index]),),
+  //           );
+  //         }),
+  //
+  //   );
+
+//  }
+//}
--- lib/ListPage.dart
+++ lib/ListPage.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(home: ListPage()));
+}
+
 class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("MY Pet Store")),
       body: ListView(
-        children:  const [
+        children:   [
           Card(
+            //elevation: 30,
+            shadowColor: Colors.red,
             color: Colors.green,
-            shadowColor: Colors.black,
             child: ListTile(
               title: Text("PetStore 1"),
               subtitle: Text("description"),
-              leading: CircleAvatar(
+              leading: const CircleAvatar(
                   backgroundImage:NetworkImage("https://static01.nyt.com/images/2020/05/09/multimedia/09sp-ai-pets-promo/09sp-ai-pets-promo-mediumSquareAt3X.jpg")),
-              trailing: Icon(Icons.phone),
+              trailing: Wrap(
+                children: const [
+                  Icon(Icons.message),
+                  SizedBox(width: 20,),
+                  Icon(Icons.phone),
+                ],
+              ),
             ),
           ),
-          ListTile(
+          const ListTile(
             title: Text("PetStore 1"),
             subtitle: Text("description"),
             leading: Image(image: NetworkImage("https://static01.nyt.com/images/2020/05/09/multimedia/09sp-ai-pets-promo/09sp-ai-pets-promo-mediumSquareAt3X.jpg")),
