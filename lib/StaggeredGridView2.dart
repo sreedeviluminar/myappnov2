3c1468592e9541915989e516e4a19c67205109d6
Sreedevi <sreedevi@luminartechnolab.com>
2022-12-29 10:59:47 +0530

    commit

--- lib/StaggeredGridView2.dart
+++ lib/StaggeredGridView2.dart
@@ -13,52 +13,94 @@ class StaggeredGridView2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: StaggeredGrid.count(
-      crossAxisCount: 4,
-      mainAxisSpacing: 4,
-      crossAxisSpacing: 4,
-      children: [
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 2,
-          child: Customchild(
-            iconData: Icons.add,
-            bgcolor: Colors.red,
-          ),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 1,
-          child: Customchild(
-            iconData: Icons.add,
-            bgcolor: Colors.red,
-          ),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 1,
-          mainAxisCellCount: 1,
-          child: Customchild(
-            iconData: Icons.add,
-            bgcolor: Colors.red,
-          ),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 1,
-          mainAxisCellCount: 1,
+        body: SingleChildScrollView(
+      child: StaggeredGrid.count(
+        crossAxisCount: 4,
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        children: [
+          StaggeredGridTile.count(
+            crossAxisCellCount: 2,
+            mainAxisCellCount: 2,
             child: Customchild(
               iconData: Icons.add,
               bgcolor: Colors.red,
             ),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 4,
-          mainAxisCellCount: 2,
-          child: Customchild(
-            iconData: Icons.add,
-            bgcolor: Colors.red,
           ),
-        ),
-      ],
+          StaggeredGridTile.count(
+            crossAxisCellCount: 2,
+            mainAxisCellCount: 1,
+            child: Customchild(
+              iconData: Icons.add,
+              bgcolor: Colors.red,
+            ),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Customchild(
+              iconData: Icons.add,
+              bgcolor: Colors.red,
+            ),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Customchild(
+              iconData: Icons.add,
+              bgcolor: Colors.red,
+            ),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 4,
+            mainAxisCellCount: 2,
+            child: Customchild(
+              iconData: Icons.add,
+              bgcolor: Colors.red,
+            ),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 2,
+            mainAxisCellCount: 2,
+            child: Customchild(
+              iconData: Icons.add,
+              bgcolor: Colors.red,
+            ),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 2,
+            mainAxisCellCount: 1,
+            child: Customchild(
+              iconData: Icons.add,
+              bgcolor: Colors.red,
+            ),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Customchild(
+              iconData: Icons.add,
+              bgcolor: Colors.red,
+            ),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Customchild(
+              iconData: Icons.add,
+              bgcolor: Colors.red,
+            ),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 4,
+            mainAxisCellCount: 2,
+            child: Customchild(
+              iconData: Icons.add,
+              bgcolor: Colors.red,
+            ),
+          ),
+        ],
+      ),
     ));
   }
 }
