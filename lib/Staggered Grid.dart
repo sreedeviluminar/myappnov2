51214cded35c245541270a66ba8883b6352da956
Sreedevi <sreedevi@luminartechnolab.com>
2022-12-29 10:42:29 +0530

    commit

---
+++ lib/Staggered Grid.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
+
+void main(){
+  runApp(MaterialApp(home: StaggeredGrid1(),));
+}
+class StaggeredGrid1 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: StaggeredGridView.count(
+          crossAxisCount: 4,
+          mainAxisSpacing: 4,
+          crossAxisSpacing: 4,
+          staggeredTiles: const [
+            StaggeredTile.count(1, 1),
+            StaggeredTile.count(1, 1),
+            StaggeredTile.count(2, 3),
+            StaggeredTile.count(2, 4),
+            StaggeredTile.count(3, 3),
+            StaggeredTile.count(2, 2),
+            StaggeredTile.count(2, 3),
+            StaggeredTile.count(2, 1),
+          ],
+          children:[
+            Customchild(iconData: Icons.add, bgcolor: Colors.red,),
+            Customchild(iconData: Icons.ac_unit, bgcolor: Colors.green,),
+            Customchild(iconData: Icons.bubble_chart, bgcolor: Colors.yellow,),
+            Customchild(iconData: Icons.hearing, bgcolor: Colors.blue,),
+            Customchild(iconData: Icons.share_arrival_time, bgcolor: Colors.orange,),
+            Customchild(iconData: Icons.tab, bgcolor: Colors.grey,),
+            Customchild(iconData: Icons.vibration, bgcolor: Colors.purple,),
+            Customchild(iconData: Icons.gamepad, bgcolor: Colors.brown,),
+
+          ]
+        ),
+    );
+  }
+}
+
+class Customchild extends StatelessWidget{
+  final Color? bgcolor;
+  final IconData iconData;
+//  VoidCallback onpress;
+
+  Customchild({required this.bgcolor,required this.iconData});
+
+  @override
+  Widget build(BuildContext context) {
+     return Card(
+       color: bgcolor,
+       child: Icon(iconData,color: Colors.white,),
+     );
+  }
+}
---
+++ lib/StaggeredGridView2.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+import 'Staggered Grid.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: StaggeredGridView2(),
+  ));
+}
+
+class StaggeredGridView2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: StaggeredGrid.count(
+      crossAxisCount: 4,
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      children: [
+        StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 2,
+          child: Customchild(
+            iconData: Icons.add,
+            bgcolor: Colors.red,
+          ),
+        ),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 1,
+          child: Customchild(
+            iconData: Icons.add,
+            bgcolor: Colors.red,
+          ),
+        ),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 1,
+          mainAxisCellCount: 1,
+          child: Customchild(
+            iconData: Icons.add,
+            bgcolor: Colors.red,
+          ),
+        ),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 1,
+          mainAxisCellCount: 1,
+            child: Customchild(
+              iconData: Icons.add,
+              bgcolor: Colors.red,
+            ),
+        ),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 4,
+          mainAxisCellCount: 2,
+          child: Customchild(
+            iconData: Icons.add,
+            bgcolor: Colors.red,
+          ),
+        ),
+      ],
+    ));
+  }
+}
