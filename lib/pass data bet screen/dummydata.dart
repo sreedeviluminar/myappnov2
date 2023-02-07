d636414d71883009a5a0a3f371d7a47972b61eb0
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-02-07 15:41:32 +0530

    commit

---
+++ lib/pass data bet screen/dummydata.dart
@@ -0,0 +1,33 @@
+const List<Map<String,dynamic>> dummyproducts = [
+  {
+    "id" : 1,
+    "name":"Product 1",
+    "description":"Very nice Product with name product 1",
+    "image" : "assets/images/imag.jpg"
+  },
+  {
+    "id" : 2,
+    "name":"Product 2",
+    "description":"Very nice Product with name product 2",
+    "image" : "assets/images/imag.jpg"
+  },
+
+  {
+    "id" : 3,
+    "name":"Product 3",
+    "description":"Very nice Product with name product 3",
+    "image" : "assets/images/imag.jpg"
+  },
+  {
+    "id" : 4,
+    "name":"Product 4",
+    "description":"Very nice Product with name product 4",
+    "image" : "assets/images/imag.jpg"
+  },
+  {
+    "id" : 5,
+    "name":"Product 5",
+    "description":"Very nice Product with name product 5",
+    "image" : "assets/images/imag.jpg"
+  },
+];
