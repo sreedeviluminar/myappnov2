47830a119734623f67f709835542aebb7e177311
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-02-07 15:41:08 +0530

    commit

--- lib/Staggered Grid.dart
+++ lib/Staggered Grid.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
 import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
 
-void main(){
-  runApp(MaterialApp(home: StaggeredGrid1(),));
+void main() {
+  runApp(MaterialApp(
+    home: StaggeredGrid1(),
+  ));
 }
-class StaggeredGrid1 extends StatelessWidget {
 
+class StaggeredGrid1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: StaggeredGridView.count(
+      body: StaggeredGridView.count(
           crossAxisCount: 4,
           mainAxisSpacing: 4,
           crossAxisSpacing: 4,
@@ -24,33 +26,60 @@ class StaggeredGrid1 extends StatelessWidget {
             StaggeredTile.count(2, 3),
             StaggeredTile.count(2, 1),
           ],
-          children:[
-            Customchild(iconData: Icons.add, bgcolor: Colors.red,),
-            Customchild(iconData: Icons.ac_unit, bgcolor: Colors.green,),
-            Customchild(iconData: Icons.bubble_chart, bgcolor: Colors.yellow,),
-            Customchild(iconData: Icons.hearing, bgcolor: Colors.blue,),
-            Customchild(iconData: Icons.share_arrival_time, bgcolor: Colors.orange,),
-            Customchild(iconData: Icons.tab, bgcolor: Colors.grey,),
-            Customchild(iconData: Icons.vibration, bgcolor: Colors.purple,),
-            Customchild(iconData: Icons.gamepad, bgcolor: Colors.brown,),
-          ]
-        ),
+          children: [
+            Customchild(
+              iconData: Icons.add,
+              bgcolor: Colors.red,
+            ),
+            Customchild(
+              iconData: Icons.ac_unit,
+              bgcolor: Colors.green,
+            ),
+            Customchild(
+              iconData: Icons.bubble_chart,
+              bgcolor: Colors.yellow,
+            ),
+            Customchild(
+              iconData: Icons.hearing,
+              bgcolor: Colors.blue,
+            ),
+            Customchild(
+              iconData: Icons.share_arrival_time,
+              bgcolor: Colors.orange,
+            ),
+            Customchild(
+              iconData: Icons.tab,
+              bgcolor: Colors.grey,
+            ),
+            Customchild(
+              iconData: Icons.vibration,
+              bgcolor: Colors.purple,
+            ),
+            Customchild(
+              iconData: Icons.gamepad,
+              bgcolor: Colors.brown,
+            ),
+          ]),
     );
   }
 }
 
-class Customchild extends StatelessWidget{
+class Customchild extends StatelessWidget {
   final Color? bgcolor;
   final IconData iconData;
+
 //  VoidCallback onpress;
 
-  Customchild({required this.bgcolor,required this.iconData});
+  Customchild({required this.bgcolor, required this.iconData});
 
   @override
   Widget build(BuildContext context) {
-     return Card(
-       color: bgcolor,
-       child: Icon(iconData,color: Colors.white,),
-     );
+    return Card(
+      color: bgcolor,
+      child: Icon(
+        iconData,
+        color: Colors.white,
+      ),
+    );
   }
 }
