9971f477435e6ed5c25e9a2875476ba822608039
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-02-15 11:58:12 +0530

    commit

---
+++ lib/sqflite1/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:myappnov2/sqflite1/sqloperation.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeStorage(),
+  ));
+}
+
+class HomeStorage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomeStorageState();
+}
+
+class HomeStorageState extends State {
+  bool isloading = true;
+  List<Map<String, dynamic>> datas = [];
+
+  void refreshdata() async {
+    final data = await SqlHelper.getItems();
+    setState(() {
+      datas = data;
+      if (datas != null) {
+        isloading = false;
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    refreshdata();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sqflite Demo'),
+      ),
+      body: isloading
+          ? const CircularProgressIndicator()
+          : ListView.builder(
+        itemCount: datas.length,
+          itemBuilder: (context, int) {
+              return  Card(
+                child: ListTile(
+                  title: Text(datas[int]['title']),
+                  subtitle: Text(datas[int]['description']),
+                ),
+              );
+            }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showform(null),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  final title_controller = TextEditingController();
+  final description_controller = TextEditingController();
+
+  void showform(int? id) async {
+    if (id != null) {
+      //id == null  create new   id != null   update
+      final existingData = datas.firstWhere((element) => element[id] == id);
+      title_controller.text       = existingData['title'];
+      description_controller.text = existingData['description'];
+    }
+    showModalBottomSheet(
+        context: context,
+        elevation: 5,
+        isScrollControlled: true,
+        builder: (context) => Container(
+              padding: EdgeInsets.only(
+                left: 15,
+                right: 15,
+                top: 15,
+                bottom: MediaQuery.of(context).viewInsets.bottom + 120,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    controller: title_controller,
+                    decoration: const InputDecoration(hintText: "Title"),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: description_controller,
+                    decoration: const InputDecoration(hintText: "Description"),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        if (id == null) {
+                          await createItem();
+                        }
+                        if (id != null) {
+                          //  await updateItem();
+                        }
+                        title_controller.text = '';
+                        description_controller.clear();
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(id == null ? 'Create New' : "Update"))
+                ],
+              ),
+            ));
+  }
+
+ Future<void> createItem() async {
+    await SqlHelper.create_item(title_controller.text , description_controller.text);
+    refreshdata();
+ }
+}
---
+++ lib/sqflite1/sqloperation.dart
@@ -0,0 +1,33 @@
+import 'package:sqflite/sqflite.dart' as sql;
+
+class SqlHelper {
+
+  static Future<void> createTable(sql.Database database) async {
+    await database.execute("""CREATE TABLE items(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        title TEXT,
+        description TEXT,
+        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )""");
+  }
+
+  static Future<sql.Database> db() async {
+    return sql.openDatabase("mydatabase.db", version: 1,
+        onCreate: (sql.Database database, int version) async {
+      await createTable(database);
+    });
+  }
+
+  static Future<List<Map<String, dynamic>>> getItems() async {
+    final db = await SqlHelper.db();
+    return db.query('items', orderBy: 'id');
+  }
+
+  static Future<int> create_item(String title, String description) async {
+    final db = await SqlHelper.db();
+    final data = {'title': title, 'description': description};
+    final id = await db.insert('items', data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    return id;
+  }
+}
